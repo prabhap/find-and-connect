477dda898a1dbbd33b7865cabf90f3c47f114aac
Prabha Pattabiraman <prabhap@thoughtworks.com>
2015-03-06 16:10:47 +0530

    Prabha - rails basic files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindAndConnect::Application.config.secret_key_base = '21e71e504d8ab7fc62e3b465246131be9b3a93a3568c42ace4c6ed5fcf70a35c908aed57f2f272943845f2af0281ddec818a3e70d477bfa6b201b2149ce61f8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FindAndConnect::Application.config.session_store :cookie_store, key: '_find-and-connect_session'
